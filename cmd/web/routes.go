11eab2cebc5a9fbde1ba43665d819c4a8668a13b
k3vwdd <kevwdiaz@outlook.com>
2025-03-22 19:37:19 -0500

    feature: middleware created and working

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,20 +4,23 @@ package main
 import (
     "net/http"
 	"github.com/go-chi/chi/v5"
+    "github.com/justinas/alice"
 
 )
 
-func (app *application) routes() *chi.Mux {
+func (app *application) routes() http.Handler {
 
     r := chi.NewRouter()
 
     fileserver := http.FileServer(http.Dir("./ui/static/"))
     r.Handle("GET /static/*", http.StripPrefix("/static", fileserver))
-
     r.Get("/", app.home)
     r.Get("/snippet/view/{id}", app.snippetView)
     r.Get("/snippet/create", app.snippetCreate)
     r.Post("/snippet/create", app.snippetCreatePost)
 
-    return r
+    standardMW := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+    return standardMW.Then(r)
+
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.5
 require (
 	github.com/go-chi/chi/v5 v5.2.1
 	github.com/go-sql-driver/mysql v1.9.0
+	github.com/justinas/alice v1.2.0
 )
 
 require filippo.io/edwards25519 v1.1.0 // indirect
